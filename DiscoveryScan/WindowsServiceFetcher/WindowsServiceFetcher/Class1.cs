0fec2b7ee37028e760829bbf94a52c3a8027c4c9
Sandhya <102655180+Sandhyakalaichelvan@users.noreply.github.com>
2024-07-15 18:23:47 +0530

    Add files via upload

---
+++ DiscoveryScan/WindowsServiceFetcher/WindowsServiceFetcher/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+
+
+namespace WindowsServiceFetcher
+{
+    public class ServiceInfo
+    {
+      
+        public string Name { get; set; }
+        public ServiceControllerStatus Status { get; set; }
+    }
+
+    public class ServiceFetcher
+    {
+        public static List<ServiceInfo> GetDuckCreekServices(string remoteServer, string username, string password)
+        {
+            List<ServiceInfo> duckCreekServices = new List<ServiceInfo>();
+
+            ServiceController[] serviceControllers = ServiceController.GetServices();
+            foreach (ServiceController serviceController in serviceControllers)
+            {
+                if (serviceController.ServiceName.StartsWith("Device", StringComparison.OrdinalIgnoreCase))
+                {
+                    ServiceInfo serviceInfo = new ServiceInfo
+                    {
+                       
+                        Name = serviceController.ServiceName,
+                        Status = serviceController.Status
+                    };
+
+                    duckCreekServices.Add(serviceInfo);
+                }
+            }
+
+            return duckCreekServices;
+        }
+    }
+
+}
